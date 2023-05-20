fcd5c64ff0c433c871abc64badf8dec5a081d14a
Mahabub Alam <mahabubx7@gmail.com>
2023-05-21 01:54:35 +0600

    :hammer: dev:refine-pre-setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useDispatch, useSelector } from "react-redux";
-import { increment } from "./redux/counter/counterSlice";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "../public/vite.svg";
-import "./App.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import { increment } from "./redux/counter/counterSlice";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+import AppRouter from "./pages/AppRouter";
 
 function App() {
-  const dispatch = useDispatch();
-  const { count } = useSelector((state) => state.counter);
+  // const dispatch = useDispatch();
+  // const { count } = useSelector((state) => state.counter);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => dispatch(increment())}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
