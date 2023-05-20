5dd715f710f3556c8a92c0bd27f9ef911e07cf58
Mahabub Alam <mahabubx7@gmail.com>
2023-05-21 02:08:18 +0600

    Revert ":hammer: dev:removed-test-console-warns"
    This reverts commit 3799ce8836ddfa660c15447f986723414a764472.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@
 import colors from "tailwindcss/colors";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     colors: {
