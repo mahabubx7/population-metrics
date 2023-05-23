c894f551f0fe082387d859455434632efd9f59da
Mahabub Alam <mahabubx7@gmail.com>
2023-05-23 14:58:08 +0600

    :rocket: component:refactored-structure-and-added-list-organ

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import propTypes from "prop-types";
-import Button from "../molecules/Button";
-import NavMenu from "../molecules/NavMenu";
-import "../styles/header.css";
+import Button from "./molecules/Button";
+import List from "./organisms/List";
+import "./styles/header.css";
 
 const Header = ({ isDetail }) => (
   <header className="header">
     {isDetail ? <Button icon="arrow-left-s-line" classes="btn-nav" /> : null}
     <h4>population</h4>
-    <NavMenu />
+    <List
+      classes="btn-icons"
+      list={[{ icon: "settings-3-line" }, { icon: "mic-line" }]}
+    />
   </header>
 );
 
---
+++ src/components/organisms/List.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import propTypes from "prop-types";
+import { v4 as uuid } from "uuid";
+import ListItem from "../molecules/ListItem";
+
+const List = ({ classes, label, list }) => (
+  <>
+    {label && <strong>{label}</strong>}
+    <ul className={classes}>
+      {list.length > 0 ? (
+        list.map((item) => (
+          <ListItem
+            key={uuid()}
+            label={item.label}
+            classes={item.cla}
+            icon={item.icon}
+            element={item.element}
+          />
+        ))
+      ) : (
+        <li className="no-items">Empty list!</li>
+      )}
+    </ul>
+  </>
+);
+
+List.propTypes = {
+  classes: propTypes.string,
+  label: propTypes.string,
+  list: propTypes.arrayOf(
+    propTypes.shape({
+      icon: propTypes.string,
+      classes: propTypes.string,
+      label: propTypes.string,
+      element: propTypes.element,
+    })
+  ),
+};
+
+List.defaultProps = {
+  classes: "",
+  label: "",
+  list: [],
+};
+
+export default List;
