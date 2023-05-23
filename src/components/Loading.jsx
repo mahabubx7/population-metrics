6c6a62f78be2df44eab35e80f4fa210f74ddd48a
Mahabub Alam <mahabubx7@gmail.com>
2023-05-24 02:53:28 +0600

    :hammer: styles-and-calculate-helper

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import reactLogo from "../assets/react.svg";
+
+const Loading = () => (
+  <span className="loader">
+    <img alt="loading" src={reactLogo} />
+  </span>
+);
+
+export default Loading;
