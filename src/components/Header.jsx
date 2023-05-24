5c354822e8931c39d1ac0559d113f998c1d40ae0
Mahabub Alam <mahabubx7@gmail.com>
2023-05-24 16:00:49 +0600

    :robot: tdd:added-Header-tests

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import List from "./organisms/List";
 import "./styles/header.css";
 
 const Header = ({ isDetail, title }) => (
-  <header className="header">
+  <header className="header" data-testid="header">
     {isDetail === true ? (
-      <Link to="/">
+      <Link to="/" data-testid="btn-back">
         <Button icon="arrow-left-s-line" classes="btn-nav" />
       </Link>
     ) : (
